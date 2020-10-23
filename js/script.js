d1e901af55fc4722892ba266eebc088b624d8663
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-10-23 16:38:33 -0400

    adds see all cards functionality

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// IPO Input -> Process -> Output
+
+// Constants and State Variables (Data)
+
+// Constant Data
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+//State Data
+let pokemonData;
+
+// Cached Element References
+const $collection = $('#collection');
+
+// Attached Event Listeners
+$collection.on('click', 'article.card', handleClick);
+
+// Functions
+
+// call immediately
+init();
+
+function init() {
+    //handles initial tasks when program starts
+    getData();
+}
+
+function getData() {
+    // fetch data using AJAX
+    $.ajax(BASE_URL).then(function (data) {
+        // take the returned data and assign to a global state variable
+        pokemonData = data;
+        // call render to visualize to the DOM
+        render();
+    }, function (error) {
+        console.log('error: ', data);
+    })
+}
+
+function handleClick() {
+    alert('a card was clicked');
+}
+
+function render() {
+    // map over the objects inside of the pokemonData results array
+    // dynamically generate html for each element in the array
+    // add that html to our collection element
+    const htmlArray = pokemonData.results.map(pokemon => {
+        return `
+        <article class="card flex-ctr">
+            <h3>${pokemon.name}</h3>
+        </article>`;
+    });
+
+    $collection.html(htmlArray);
+};
